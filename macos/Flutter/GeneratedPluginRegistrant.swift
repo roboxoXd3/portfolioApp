ffbb782132c761bff0fee2f850f8237ec33f775e
roboxoXd3 <rishavshahi87@gmail.com>
2024-07-08 22:37:06 +0530

    first commit

---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_gl_macos
+import rive_common
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterGlMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterGlMacosPlugin"))
+  RivePlugin.register(with: registry.registrar(forPlugin: "RivePlugin"))
+}
---
+++ windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,17 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <flutter_gl_windows/flutter_gl_windows_plugin.h>
+#include <rive_common/rive_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FlutterGlWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterGlWindowsPlugin"));
+  RivePluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("RivePlugin"));
+}
